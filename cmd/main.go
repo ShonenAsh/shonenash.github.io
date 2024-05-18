58b8c23f00351a764b4174e26ffb0d544a02b0f8
ShonenAsh <shonenash@gmail.com>
2024-05-18 12:20:11 -0400

    htmx abo to get about.html

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	dir := http.Dir("./..")
 	http.Handle("/", http.FileServer(dir))
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
